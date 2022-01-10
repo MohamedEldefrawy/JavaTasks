e2892b0e2cc009d1a90526c63fe0f7de1f088f5b
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-10 21:52:50 +0200

    Finish drawing stars task

--- Lab 2/DrawingStars/DrawingStars.java
+++ Lab 2/DrawingStars/DrawingStars.java
@@ -1,4 +1,44 @@
 public class DrawingStars
 {
+	private int height;
+	private int spaceBetweenCount = 100;
 
+
+	public DrawingStars()
+	{
+		this.height = 4;
+	}
+
+	public DrawingStars(int height)
+	{
+		this.height = height;
+	}
+
+
+	public void draw()
+	{
+		for(int i = 0; i<height; i++)
+		{
+			for(int j=0; j<i; j++)
+			{
+				System.out.print("* ");	
+							
+			}
+			
+			for(int k=spaceBetweenCount; k>0; k--)
+			{
+			 	System.out.print(" ");				
+			}
+					
+
+			for(int l=0; l<i; l++)
+			{
+				System.out.print("* ");					
+			}
+
+
+			System.out.println("\n");
+			spaceBetweenCount -= 3;		
+		}	
+	}
 }
--- Lab 2/DrawingStars/Main.java
+++ Lab 2/DrawingStars/Main.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class Main
 {
 	public static void main (String[] args)
 	{
-
+		System.out.println("Please enter the height of the star");
+		Scanner sc = new Scanner(System.in);
+		int height = sc.nextInt();
+		DrawingStars drawingStars = new DrawingStars(height);
+		drawingStars.draw();
 	}
 }
